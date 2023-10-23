878b1d22cc1be64b9fbb4e01b1ab89dd8f32dbe0
Anton Mamontov <a.mamontov@p-s.kz>
2023-10-23 14:04:56 +0600

    adding title changing

--- src/components/TodoItem.tsx
+++ src/components/TodoItem.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { useAppDispatch } from "../hooks";
 import { removeTodo, updateTodo } from "../redux/Slices/TodoSlice";
 
@@ -10,6 +10,18 @@ interface TodoItemProps {
 
 const TodoItem: React.FC<TodoItemProps> = ({ id, title, completed }) => {
   const dispatch = useAppDispatch();
+
+  const [edit, setEdit] = useState(false);
+
+  const [currentTitle, setCurrentTitle] = useState(title);
+
+  const formSubmit = (e: { preventDefault: () => void }) => {
+    e.preventDefault();
+
+    setEdit((prev) => !prev);
+
+    dispatch(updateTodo({ id: id, changes: { title: currentTitle } }));
+  };
   return (
     <li key={id}>
       <input
@@ -24,7 +36,16 @@ const TodoItem: React.FC<TodoItemProps> = ({ id, title, completed }) => {
           )
         }
       />
-      <span>{title}</span>
+      <span style={{ display: edit ? "none" : "" }}>{title}</span>
+      <form onSubmit={formSubmit} style={{ display: edit ? "inline" : "none" }}>
+        <input
+          value={currentTitle}
+          onChange={(e) => setCurrentTitle(e.target.value)}
+        />
+      </form>
+      <span className="span-button" onClick={() => setEdit((prev) => !prev)}>
+        Change
+      </span>
       <span className="span-button" onClick={() => dispatch(removeTodo(id))}>
         DEL
       </span>
