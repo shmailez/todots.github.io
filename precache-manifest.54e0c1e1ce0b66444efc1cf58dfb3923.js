ec6065b3a21dbc59b9ba2935deaef1cdb00fa2dd
Anton Mamontov <a.mamontov@p-s.kz>
2023-11-06 20:41:24 +0600

    Updates

---
+++ precache-manifest.54e0c1e1ce0b66444efc1cf58dfb3923.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b6f271ed3ffe5e86f9a2c17c128b5817",
+    "url": "/shmailez/todots.github.io/index.html"
+  },
+  {
+    "revision": "48f1cb999c7fdf4d7105",
+    "url": "/shmailez/todots.github.io/static/css/main.68ab3738.chunk.css"
+  },
+  {
+    "revision": "00b2bf974b7850c5158c",
+    "url": "/shmailez/todots.github.io/static/js/2.0c94ef94.chunk.js"
+  },
+  {
+    "revision": "03c2247ebef71b42a125",
+    "url": "/shmailez/todots.github.io/static/js/3.133d7e0c.chunk.js"
+  },
+  {
+    "revision": "48f1cb999c7fdf4d7105",
+    "url": "/shmailez/todots.github.io/static/js/main.83db09f9.chunk.js"
+  },
+  {
+    "revision": "529450c730d849446dfd",
+    "url": "/shmailez/todots.github.io/static/js/runtime~main.c2ee0e43.js"
+  }
+]);
